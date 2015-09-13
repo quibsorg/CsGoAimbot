2759f4315a457263d07cdd53390383788bddb570
Joakim Carlsson <joakimcarlsson94@hotmail.com>
2015-09-13 17:34:02 +0200

    Removed unused stuff from proram like ESP

--- CsGoApplicationAimbot/CsGoApplicationAimbot/Program.cs
+++ CsGoApplicationAimbot/CsGoApplicationAimbot/Program.cs
@@ -1,19 +1,11 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Windows.Forms;
-using CsGoApplicationAimbot.CSGO.Enums;
 using CsGoApplicationAimbot.CSGOClasses;
 using ExternalUtilsCSharp;
 using ExternalUtilsCSharp.SharpDXRenderer;
-using ExternalUtilsCSharp.SharpDXRenderer.Controls;
-using ExternalUtilsCSharp.SharpDXRenderer.Controls.Crosshairs;
-using ExternalUtilsCSharp.SharpDXRenderer.Controls.Layouts;
-using ExternalUtilsCSharp.UI.UIObjects;
-using SharpDX.DirectWrite;
 
 namespace CsGoApplicationAimbot
 {
@@ -27,7 +19,6 @@ namespace CsGoApplicationAimbot
         #region VARIABLES
         public static KeyUtils KeyUtils;
         private static IntPtr _hWnd;
-        private static double _seconds = 0;
         public static Framework Framework;
         public static ProcUtils ProcUtils;
         public static MemUtils MemUtils;
@@ -36,94 +27,6 @@ namespace CsGoApplicationAimbot
 
         #region CONTROLS
         public static SharpDXOverlay ShdxOverlay;
-
-        private static SharpDXCursor _cursor;
-        //Menu-window
-        private static SharpDXWindow _windowMenu;
-        private static SharpDXTabControl _tabsMenu;
-
-        private static SharpDXLabel _labelHotkeys;
-        private static SharpDXPanel _panelEspContent;
-        private static SharpDXCheckBox _checkBoxEspEnabled;
-        private static SharpDXCheckBox _checkBoxEspBox;
-        private static SharpDXCheckBox _checkBoxEspSkeleton;
-        private static SharpDXCheckBox _checkBoxEspName;
-        private static SharpDXCheckBox _checkBoxEspHealth;
-        private static SharpDXCheckBox _checkBoxEspAllies;
-        private static SharpDXCheckBox _checkBoxEspEnemies;
-
-        private static SharpDXPanel _panelAimContent;
-        private static SharpDXCheckBox _checkBoxAimEnabled;
-        private static SharpDXCheckBox _checkBoxAimDrawFov;
-        private static SharpDXCheckBox _checkBoxAimFilterSpotted;
-        private static SharpDXCheckBox _checkBoxAimFilterSpottedBy;
-        private static SharpDXCheckBox _checkBoxAimFilterEnemies;
-        private static SharpDXCheckBox _checkBoxAimFilterAllies;
-        private static SharpDXRadioButton _radioAimToggle;
-        private static SharpDXRadioButton _radioAimHold;
-        private static SharpDXTrackbar _trackBarAimFov;
-        private static SharpDXCheckBox _checkBoxAimSmoothEnaled;
-        private static SharpDXTrackbar _trackBarAimSmoothValue;
-        private static SharpDXButtonKey _keyAimKey;
-        private static SharpDXComboValue<int> _comboValueAimBone;
-
-        private static SharpDXPanel _panelRcsContent;
-        private static SharpDXCheckBox _checkBoxRcsEnabled;
-        private static SharpDXTrackbar _trackBarRcsForce;
-
-        private static SharpDXPanel _panelTriggerContent;
-        private static SharpDXCheckBox _checkBoxTriggerEnabled;
-        private static SharpDXCheckBox _checkBoxTriggerFilterEnemies;
-        private static SharpDXCheckBox _checkBoxTriggerFilterAllies;
-        private static SharpDXRadioButton _radioTriggerToggle;
-        private static SharpDXRadioButton _radioTriggerHold;
-        private static SharpDXButtonKey _keyTriggerKey;
-        private static SharpDXTrackbar _trackBarTriggerDelayFirstShot;
-        private static SharpDXTrackbar _trackBarTriggerDelayShots;
-        private static SharpDXCheckBox _checkBoxTriggerBurstEnabled;
-        private static SharpDXCheckBox _checkBoxTriggerBurstRandomize;
-        private static SharpDXTrackbar _trackBarTriggerBurstShots;
-
-        private static SharpDXPanel _panelRadarContent;
-        private static SharpDXCheckBox _checkBoxRadarEnabled;
-        private static SharpDXCheckBox _checkBoxRadarAllies;
-        private static SharpDXCheckBox _checkBoxRadarEnemies;
-        private static SharpDXTrackbar _trackBarRadarScale;
-        private static SharpDXTrackbar _trackBarRadarWidth;
-        private static SharpDXTrackbar _trackBarRadarHeight;
-
-        private static SharpDXPanel _panelCrosshairContent;
-        private static SharpDXCheckBox _checkBoxCrosshairEnabled;
-        private static SharpDXTrackbar _trackBarCrosshairRadius;
-        private static SharpDXTrackbar _trackBarCrosshairWidth;
-        private static SharpDXTrackbar _trackBarCrosshairSpreadScale;
-        private static SharpDXCheckBox _checkBoxCrosshairOutline;
-        private static SharpDXComboValue<int> _comboValueCrosshairType;
-        private static SharpDXColorControl _colorControlCrosshairPrimary;
-        private static SharpDXColorControl _colorControlCrosshairSecondary;
-
-        private static SharpDXPanel _panelWindows;
-        private static SharpDXCheckBox _checkBoxGraphsEnabled;
-        private static SharpDXCheckBox _checkBoxSpectatorsEnabled;
-        private static SharpDXCheckBox _checkBoxBotsEnabled;
-        private static SharpDXCheckBox _checkBoxEnemiesEnabled;
-
-        //Performance-window
-        private static SharpDXWindow _windowGraphs;
-        private static SharpDXGraph _graphMemRead;
-        private static SharpDXGraph _graphMemWrite;
-
-        //Spectators-window
-        private static SharpDXWindow _windowSpectators;
-        private static SharpDXLabel _labelSpectators;
-
-        //Aimbot/Triggerbot window
-        private static SharpDXWindow _windowBots;
-        private static SharpDXLabel _labelAimbot;
-        private static SharpDXLabel _labelTriggerbot;
-
-        //Others
-        private static Crosshair _ctrlCrosshair;
         #endregion
 
         #region METHODS
@@ -225,7 +128,6 @@ namespace CsGoApplicationAimbot
 
         private static void overlay_TickEvent(object sender, SharpDXOverlay.DeltaEventArgs e)
         {
-            _seconds += e.SecondsElapsed;
             KeyUtils.Update();
             Framework.Update();
             ShdxOverlay.UpdateControls(e.SecondsElapsed, KeyUtils);
@@ -258,45 +160,6 @@ namespace CsGoApplicationAimbot
             //{
             //    _labelSpectators.Text = "<none>";
             //}
-        }
-
-        static void checkBox_CheckedChanged(object sender, EventArgs e)
-        {
-            SharpDXCheckBox control = (SharpDXCheckBox)sender;
-            ConfigUtils.SetValue(control.Tag.ToString(), control.Checked);
-        }
-        private static void radioButton_CheckedChanged(object sender, EventArgs e)
-        {
-            SharpDXRadioButton control = (SharpDXRadioButton)sender;
-            ConfigUtils.SetValue(control.Tag.ToString(), control.Checked);
-        }
-        private static void trackBar_ValueChangedEvent(object sender, EventArgs e)
-        {
-            SharpDXTrackbar control = (SharpDXTrackbar)sender;
-            ConfigUtils.SetValue(control.Tag.ToString(), control.Value);
-        }
-        static void buttonKey_KeyChangedEvent(object sender, EventArgs e)
-        {
-            SharpDXButtonKey control = (SharpDXButtonKey)sender;
-            ConfigUtils.SetValue(control.Tag.ToString(), control.Key);
-        }
-        static void button_MouseClickEventUp(object sender, ExternalUtilsCSharp.UI.Control<SharpDXRenderer, SharpDX.Color, SharpDX.Vector2, TextFormat>.MouseEventArgs e)
-        {
-            if (!e.LeftButton)
-                return;
-            SharpDXPanel panel = (SharpDXPanel)((SharpDXButton)sender).Tag;
-            panel.Visible = !panel.Visible;
-        }
-
-        private static void comboValue_SelectedIndexChangedEvent<T>(object sender, SharpDXComboValue<T>.ComboValueEventArgs e)
-        {
-            ConfigUtils.SetValue(e.Tag.ToString(), e.Value);
-        }
-
-        static void control_ColorChangedEvent(object sender, EventArgs e)
-        {
-            SharpDXColorControl control = (SharpDXColorControl)sender;
-            ConfigUtils.SetValue(control.Tag.ToString(), control.Color.ToRGBA());
         }
         #endregion
 
