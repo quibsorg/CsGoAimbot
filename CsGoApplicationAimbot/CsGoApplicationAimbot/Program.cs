f050cfa62134bfd2d9c2586c9ca64d8ac3894598
Joakim Carlsson <JoakiMCarlsson1994@live.se>
2015-10-05 13:22:57 +0200

    Revert "Added two new methods, 1 to generate a random title. And than 1 to start our cheat."
    This reverts commit 8cd4ab1201f2a75cc78d3ea3165fb6b2efca6ac0.

--- CsGoApplicationAimbot/CsGoApplicationAimbot/Program.cs
+++ CsGoApplicationAimbot/CsGoApplicationAimbot/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Timers;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading;
 using System.Windows.Forms;
 using CsGoApplicationAimbot.CSGOClasses;
@@ -36,22 +35,6 @@ namespace CsGoApplicationAimbot
         public static void Main(string[] args)
         {
             PrintSuccess("Smurf bot");
-            //Sets a random title to our Console Window.. Almost useless.
-            Console.Title = RandomTitle(15);
-            //Starts the main core of our cheat.
-            StartCheat();
-        }
-
-        public static string RandomTitle(int length)
-        {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            var random = new Random();
-            return new string(Enumerable.Repeat(chars, length)
-              .Select(s => s[random.Next(s.Length)]).ToArray());
-        }
-
-        private static void StartCheat()
-        {
             //We make the config if it dosen't exist.
             _settings = new SettingsConfig();
             KeyUtils = new KeyUtils();
@@ -79,7 +62,7 @@ namespace CsGoApplicationAimbot
 
             _timer.Elapsed += _timer_Elapsed;
             _timer.Start();
-
+            
             PrintSuccess("Cheat is now running.");
             Application.Run();
         }
